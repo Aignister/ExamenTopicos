bb21c9c41798b37e17af5c2c7b9562ac75102701
Enrique Torres <enriquetorresv652@gmail.com>
2023-11-05 18:36:18 -0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.txtPropietario = new System.Windows.Forms.TextBox();
+            this.txtRaza = new System.Windows.Forms.TextBox();
+            this.txtEspecie = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataTable = new System.Windows.Forms.DataGridView();
+            this.panel3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(867, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 524);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(867, 41);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DimGray;
+            this.panel3.Controls.Add(this.groupBox2);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(667, 41);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 483);
+            this.panel3.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRegistrar);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.btnModificar);
+            this.groupBox2.Controls.Add(this.btnBuscar);
+            this.groupBox2.Controls.Add(this.txtId);
+            this.groupBox2.Controls.Add(this.btnEliminar);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(6, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(191, 256);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Opciones";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.Transparent;
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRegistrar.Location = new System.Drawing.Point(52, 29);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(94, 36);
+            this.btnRegistrar.TabIndex = 0;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(12, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Id:";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnModificar.Location = new System.Drawing.Point(52, 212);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(94, 36);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnBuscar.Location = new System.Drawing.Point(52, 163);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(94, 36);
+            this.btnBuscar.TabIndex = 7;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(52, 79);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(110, 26);
+            this.txtId.TabIndex = 11;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEliminar.Location = new System.Drawing.Point(52, 118);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(94, 36);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 271);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 206);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel4.Controls.Add(this.groupBox1);
+            this.panel4.Controls.Add(this.dataTable);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 41);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(667, 483);
+            this.panel4.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.dateTime);
+            this.groupBox1.Controls.Add(this.txtPropietario);
+            this.groupBox1.Controls.Add(this.txtRaza);
+            this.groupBox1.Controls.Add(this.txtEspecie);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(649, 231);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro";
+            // 
+            // dateTime
+            // 
+            this.dateTime.Location = new System.Drawing.Point(301, 74);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(287, 26);
+            this.dateTime.TabIndex = 9;
+            // 
+            // txtPropietario
+            // 
+            this.txtPropietario.Location = new System.Drawing.Point(112, 177);
+            this.txtPropietario.Name = "txtPropietario";
+            this.txtPropietario.Size = new System.Drawing.Size(153, 26);
+            this.txtPropietario.TabIndex = 8;
+            // 
+            // txtRaza
+            // 
+            this.txtRaza.Location = new System.Drawing.Point(90, 130);
+            this.txtRaza.Name = "txtRaza";
+            this.txtRaza.Size = new System.Drawing.Size(175, 26);
+            this.txtRaza.TabIndex = 7;
+            // 
+            // txtEspecie
+            // 
+            this.txtEspecie.Location = new System.Drawing.Point(90, 86);
+            this.txtEspecie.Name = "txtEspecie";
+            this.txtEspecie.Size = new System.Drawing.Size(175, 26);
+            this.txtEspecie.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(90, 39);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(175, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(8, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Propietario:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(304, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha De Nacimiento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(8, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Raza:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(7, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Especie:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(8, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // dataTable
+            // 
+            this.dataTable.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTable.Location = new System.Drawing.Point(12, 246);
+            this.dataTable.Name = "dataTable";
+            this.dataTable.Size = new System.Drawing.Size(649, 231);
+            this.dataTable.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 565);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro";
+            this.panel3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTime;
+        private System.Windows.Forms.TextBox txtPropietario;
+        private System.Windows.Forms.TextBox txtRaza;
+        private System.Windows.Forms.TextBox txtEspecie;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataTable;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TablaPacientes();
+        }
+
+        SqlConnection conexion = new SqlConnection("Data Source=DARKWURM;Initial Catalog=dbVeterinaria;Integrated Security=True");
+
+        public void Limpiar()
+        {
+            txtNombre.Text = "";
+            txtEspecie.Text = "";
+            txtRaza.Text = "";
+            txtPropietario.Text = "";
+            txtId.Text = "";
+            dateTime.Value = DateTime.Now;
+        }
+
+        public void TablaPacientes()
+        {
+            SqlDataAdapter data = new SqlDataAdapter("SELECT * FROM Paciente", conexion);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataTable.DataSource = dt;
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtEspecie.Text) || string.IsNullOrEmpty(txtRaza.Text) || string.IsNullOrEmpty(txtPropietario.Text) || dateTime.Value == null || string.IsNullOrEmpty(txtPropietario.Text))
+            {
+                MessageBox.Show("Faltan campos por llenar");
+                return; 
+            }
+
+            conexion.Open();
+
+            SqlCommand registar = new SqlCommand("Crud_Paciente", conexion);
+            registar.CommandType = CommandType.StoredProcedure;
+
+            registar.Parameters.Add(new SqlParameter("@Opc", SqlDbType.NVarChar, 30)).Value = "Ingresar";
+            registar.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int )).Value = 0;
+            registar.Parameters.Add(new SqlParameter("@Nombre", SqlDbType.NVarChar, 50)).Value = txtNombre.Text;
+            registar.Parameters.Add(new SqlParameter("@Especie", SqlDbType.NVarChar, 50)).Value = txtEspecie.Text;
+            registar.Parameters.Add(new SqlParameter("@Raza", SqlDbType.NVarChar, 50)).Value = txtRaza.Text;
+            registar.Parameters.Add(new SqlParameter("@Fecha_Nacimiento", SqlDbType.Date)).Value = dateTime.Value;
+            registar.Parameters.Add(new SqlParameter("@Propietario", SqlDbType.NVarChar, 50)).Value = txtPropietario.Text;
+
+            try
+            {
+                registar.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            conexion.Close();
+            Limpiar();
+            TablaPacientes();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+
+            SqlCommand eliminar = new SqlCommand("Crud_Paciente", conexion);
+            eliminar.CommandType = CommandType.StoredProcedure;
+
+            eliminar.Parameters.Add(new SqlParameter("@Opc", SqlDbType.NVarChar, 30)).Value = "Eliminar";
+            eliminar.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int)).Value = int.Parse(txtId.Text);
+            eliminar.Parameters.Add(new SqlParameter("@Nombre", SqlDbType.NVarChar, 50)).Value = txtNombre.Text;
+            eliminar.Parameters.Add(new SqlParameter("@Especie", SqlDbType.NVarChar, 50)).Value = txtEspecie.Text;
+            eliminar.Parameters.Add(new SqlParameter("@Raza", SqlDbType.NVarChar, 50)).Value = txtRaza.Text;
+            eliminar.Parameters.Add(new SqlParameter("@Fecha_Nacimiento", SqlDbType.Date)).Value = dateTime.Value;
+            eliminar.Parameters.Add(new SqlParameter("@Propietario", SqlDbType.NVarChar, 50)).Value = txtPropietario.Text;
+
+            try
+            {
+                int rowsAffected = eliminar.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("El Id ingresada es incorrecta o no existe");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            conexion.Close();
+            Limpiar();
+            TablaPacientes();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtEspecie.Text) || string.IsNullOrEmpty(txtRaza.Text) || string.IsNullOrEmpty(txtPropietario.Text) || dateTime.Value == null || string.IsNullOrEmpty(txtPropietario.Text))
+            {
+                MessageBox.Show("Faltan campos por llenar");
+                return;
+            }
+
+            conexion.Open();
+
+            SqlCommand modificar = new SqlCommand("Crud_Paciente", conexion);
+            modificar.CommandType = CommandType.StoredProcedure;
+
+            modificar.Parameters.Add(new SqlParameter("@Opc", SqlDbType.NVarChar, 30)).Value = "Modificar";
+            modificar.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int)).Value = int.Parse(txtId.Text);
+            modificar.Parameters.Add(new SqlParameter("@Nombre", SqlDbType.NVarChar, 50)).Value = txtNombre.Text;
+            modificar.Parameters.Add(new SqlParameter("@Especie", SqlDbType.NVarChar, 50)).Value = txtEspecie.Text;
+            modificar.Parameters.Add(new SqlParameter("@Raza", SqlDbType.NVarChar, 50)).Value = txtRaza.Text;
+            modificar.Parameters.Add(new SqlParameter("@Fecha_Nacimiento", SqlDbType.Date)).Value = dateTime.Value;
+            modificar.Parameters.Add(new SqlParameter("@Propietario", SqlDbType.NVarChar, 50)).Value = txtPropietario.Text;
+
+            try
+            {
+                modificar.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            conexion.Close();
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    TextBox text = ctrl as TextBox;
+                    text.Clear();
+                }
+            }
+                Limpiar();
+                TablaPacientes();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+
+            SqlCommand buscar = new SqlCommand("Crud_Paciente", conexion);
+            buscar.CommandType = CommandType.StoredProcedure;
+
+            buscar.Parameters.Add(new SqlParameter("@Opc", SqlDbType.NVarChar, 30)).Value = "Buscar";
+            buscar.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int)).Value = int.Parse(txtId.Text);
+            buscar.Parameters.Add(new SqlParameter("@Nombre", SqlDbType.NVarChar, 50)).Value = txtNombre.Text;
+            buscar.Parameters.Add(new SqlParameter("@Especie", SqlDbType.NVarChar, 50)).Value = txtEspecie.Text;
+            buscar.Parameters.Add(new SqlParameter("@Raza", SqlDbType.NVarChar, 50)).Value = txtRaza.Text;
+            buscar.Parameters.Add(new SqlParameter("@Fecha_Nacimiento", SqlDbType.Date)).Value = dateTime.Value;
+            buscar.Parameters.Add(new SqlParameter("@Propietario", SqlDbType.NVarChar, 50)).Value = txtPropietario.Text;
+
+            SqlDataReader leer = buscar.ExecuteReader();
+
+            if (leer.HasRows)
+            {
+                while (leer.Read())
+                {
+                    txtId.Text = leer[0].ToString();
+                    txtNombre.Text = leer[1].ToString();
+                    txtEspecie.Text = leer[2].ToString();
+                    txtRaza.Text = leer[3].ToString();
+                    txtPropietario.Text = leer[5].ToString();
+                    dateTime.Value = DateTime.Parse(leer[4].ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("El Id ingresada es incorrecta o no existe");
+            }
+
+            conexion.Close();
+        }
+    }
+}
